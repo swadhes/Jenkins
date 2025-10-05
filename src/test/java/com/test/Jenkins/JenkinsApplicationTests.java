27ee831d04129325e8d65acc7e3b5aa32b6f78fb
Swadheen Sethy <swadheensethy@Swadheens-MacBook-Pro.local>
2025-10-05 20:27:03 +0530

    Jenkins file fixed

--- src/test/java/com/test/Jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/test/Jenkins/JenkinsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.test.Jenkins;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -8,10 +9,11 @@ import org.springframework.test.context.TestPropertySource;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.properties")
+@Slf4j
 public class JenkinsApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		// Test will pass if the application context loads
+		log.info("Application context loaded successfully");
 	}
 }
